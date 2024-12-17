c2e02b208ab546f3cea32731a46a567356b522cc
loexes20145 <emaklov8145@gmail.com>
2024-12-17 17:41:18 +0000

    modified:   src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Pour les passionés de plantes</span>
+      <div>
+        <span>Laissez-nous votre email:</span>
+        <input type="text" placeholder='Entrez votre email'/>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
