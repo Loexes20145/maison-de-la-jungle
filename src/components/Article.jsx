dfd6107847166023d2d868ca3e0a1567ee241a94
loexes20145 <emaklov8145@gmail.com>
2024-12-19 17:11:56 +0000

    added: mainSection's articles components

---
+++ src/components/Article.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+const articles = [
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Montsera',
+    price: 15,
+    imageUrl: '/assets/montsera.jpg',
+    rate1: 3,
+    rate2: 2
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Ficus Lyrata',
+    price: 16,
+    imageUrl: '/assets/ficus-lyrata.png',
+    rate1: 1,
+    rate2: 3
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Pothos argenté',
+    price: 9,
+    imageUrl: '/assets/pothos-argenté.jpg',
+    rate1: 2,
+    rate2: 1
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Calathea',
+    price: 20,
+    imageUrl: '/assets/calathea.jpeg',
+    rate1: 3,
+    rate2: 2
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Olivier',
+    price: 25,
+    imageUrl: '/assets/olivier.jpg',
+    rate1: 1,
+    rate2: 3
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Cactus',
+    price: 6,
+    imageUrl: '/assets/cactus.jpeg',
+    rate1: 1,
+    rate2: 2
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Basilic',
+    price: 5,
+    imageUrl: '/assets/basilic.jpg',
+    rate1: 3,
+    rate2: 2
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Succulente',
+    price: 8,
+    imageUrl: '/assets/succulente.jpg',
+    rate1: 1,
+    rate2: 2
+  },
+  {
+    id: Math.floor(Math.random()*99999999),
+    name: 'Menthe',
+    price: 4,
+    imageUrl: '/assets/menthe.jpg',
+    rate1: 2,
+    rate2: 2
+  }
+]
+
+const Articleslist = () => {
+
+  return (
+    <>
+    {
+      articles.map(({name, price, imageUrl, rate1, rate2}) => (
+        <article className='article'>
+            <div className='article__Info'>
+              <img className='article__Info__Image' src={imageUrl} alt={name} />
+              <strong className='article__Info__Price'>{price}€</strong>
+              <p className='article__Info__Title'>{name}</p>
+            </div>
+            <div className='article__Rate'>
+              <span className='rate1'>{rate1}</span>
+              <span className='rate2'>{rate2}</span>
+            </div>
+            <div className='article__Option'>
+              <button>Ajouter +</button>
+            </div>
+        </article>
+        )
+      )
+    }
+    </>
+  )
+}
+
+export default Articleslist
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import ArticlesList from './Article.jsx'
 
 const Main = () => {
   return (
     <main className='main'>
-      <div className='main__Aside'>Votre panier est vide</div>
-      <div className='main__Section'>
-        <h2>The main section</h2>
+      <div className='mainAside'>Votre panier est vide</div>
+      <div className='mainSection'>
+        <div className='mainSection__Title'>
+          <h2>The main section</h2>
+        </div>
+        <div className='mainSection__Content'>
+          <ArticlesList />
+        </div>
       </div>
     </main>
   )
