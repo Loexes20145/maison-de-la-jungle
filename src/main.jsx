6ae9607481e250d688d77411b0760df5a32f692d
loexes20145 <emaklov8145@gmail.com>
2024-12-19 11:22:51 +0000

    added styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Main from './components/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Main />
-    <Footer />
+    <App />
   </StrictMode>,
 )
