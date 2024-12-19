d85f3159aab683b220762f21cdd0423af522294f
loexes20145 <emaklov8145@gmail.com>
2024-12-19 17:54:57 +0000

    modified: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <span>Pour les passionés de plantes</span>
-      <div>
+      <span className='footer__Text'>Pour les passionés de plantes <img src="/assets/fern.png" alt="fern" /><img src="/assets/plant.png" alt="plant" /> <img src="/assets/cactus.png" alt="cactus" /></span>
+      <div className='footer__Contact'>
         <span>Laissez-nous votre email:</span>
         <input type="text" placeholder='Entrez votre email'/>
-        </div>
+      </div>
     </footer>
   )
 }
